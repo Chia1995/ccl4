833c21c1a9aed09c85cd90bf9048d042485ab6d8
cc231027 <148882726+cc231027@users.noreply.github.com>
2025-06-11 09:35:36 +0200

    Working prototype

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,140 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+/*using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    bool alive = true;
+
+    public float speed = 5;
+    [SerializeField] Rigidbody rb;
+
+    float horizontalInput;
+    [SerializeField] float horizontalMultiplier = 2;
+
+    [SerializeField] float jumpForce = 400f;
+
+    [SerializeField] LayerMask groundMask;
+
+    // Speed increase per point collected
+    public float speedIncreasePerPoint = 0.1f;
+
+
+    private void FixedUpdate()
     {
-        
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Check for jump input
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        alive = false;
+        // Restart the game
+        Invoke("Restart", 2);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Jump()
+    {
+        // Check if the player is grounded before allowing a jump
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
+
+        // If the player is grounded, apply a jump force
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+    }
+}
+*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    bool alive = true;
+
+    [SerializeField] public float speed = 5f;
+    [SerializeField] private Rigidbody rb;
+
+    float horizontalInput;
+    [SerializeField] private float horizontalMultiplier = 2f;
+    [SerializeField] private float jumpForce = 400f;
+
+    [SerializeField] private LayerMask groundMask;
+
+    // Speed increase per healthy mushroom
+    [SerializeField] public float speedIncreasePerPoint = 0.1f;
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        alive = false;
+        Invoke("Restart", 2);
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Jump()
+    {
+        float height = GetComponent<Collider>().bounds.size.y;
+        bool isGrounded = Physics.Raycast(transform.position, Vector3.down, (height / 2) + 0.1f, groundMask);
+
+        if (isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
     }
 }
