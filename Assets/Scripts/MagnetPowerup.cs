73b6446531484a74d40a2d5d3a19c272fd3e5573
cc231027 <148882726+cc231027@users.noreply.github.com>
2025-06-11 14:12:27 +0200

    Particle effects added

--- Assets/Scripts/MagnetPowerup.cs
+++ Assets/Scripts/MagnetPowerup.cs
@@ -3,17 +3,33 @@ using UnityEngine;
 public class MagnetPowerup : MonoBehaviour
 {
     [SerializeField] private float duration = 5f;
+    [SerializeField] private GameObject pickupEffectPrefab;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Activate magnet on the player
             PlayerMagnet magnet = other.GetComponent<PlayerMagnet>();
             if (magnet != null)
             {
                 magnet.ActivateMagnet(duration);
-                Destroy(gameObject); // Destroy the power-up after pickup
             }
+
+            // Spawn and attach the pickup effect
+            if (pickupEffectPrefab != null)
+            {
+                GameObject effect = Instantiate(pickupEffectPrefab, transform.position, Quaternion.identity);
+
+                // Parent to the player so it follows them
+                effect.transform.SetParent(other.transform);
+
+                // Destroy the effect after the duration ends
+                Destroy(effect, duration);
+            }
+
+            // Destroy the magnet power-up object itself
+            Destroy(gameObject);
         }
     }
 }
