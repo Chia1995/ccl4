a4b8762234da21436ffcb30a26cec1c4c3d5b131
cc231027 <148882726+cc231027@users.noreply.github.com>
2025-06-15 12:16:03 +0200

    MainMenuScene added

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGameScene"); // Change to your actual main game scene name
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // Stops play mode in editor
+#endif
+    }
+}
