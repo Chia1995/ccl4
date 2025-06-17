bf6e3af84326d0f758e50c153cfbf5fb5732b0f3
ecemchen <ecemtasali@gmail.com>
2025-06-17 22:39:47 +0200

    FIX Event Play

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,11 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_TESTPOISON = 3578776682U;
+    } // namespace EVENTS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
